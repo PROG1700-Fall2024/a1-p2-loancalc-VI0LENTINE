446317ed74950f309b60ae8bfc16e5883bd3d970
VI0LENTINE <W0516070@campus.nscc.ca>
2024-10-03 12:20:45 -0300

    testing commit

--- A1-P2-LoanCalc.py
+++ A1-P2-LoanCalc.py
@@ -4,12 +4,19 @@
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
+    # Amount of loan, user input
+    loan = input("Enter the amount of your loan: ")
 
+    # Interest rate, user input
+    interest = input("Enter your interest rate (%): ")
+    interestDec = (float(interest)/ 5200)
 
+    # Number of years
+    years = input("Enter the amount of years: ")
 
-
-
-
+    # Weekly payment results
+    weekly = (float(interestDec))/(1-(1+float(interestDec))**(-52*int(years)))*float(loan)
+    print("Your weekly payment is: $", round(weekly, 2))
 
     # YOUR CODE ENDS HERE
 
